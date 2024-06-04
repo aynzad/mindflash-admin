123831fb5c87c7fb536ce79b46c2703828316da2
Alireza Esfahani <aynzad@gmail.com>
2024-06-04 20:41:57 +0200

    rename component typo

--- src/app/dashboard/categories/create/page.tsx
+++ src/app/dashboard/categories/create/page.tsx
@@ -1,7 +1,7 @@
 import { CreateCategoryForm } from "@/components/forms/CreateCategoryForm";
 import { PageLayout } from "@/components/page/PageLayout";
 
-export default async function CreateLocale() {
+export default async function CreateCategory() {
   return (
     <PageLayout title="Create category" description="Create new category">
       <CreateCategoryForm />
